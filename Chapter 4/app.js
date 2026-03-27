6fcb04efa27804a5bd27cf30d68f08746c355011
Mehwish Shahbaz <mahwishahbaz@gmail.com>
2026-03-27 12:17:54 +0500

    Chapter 4 Done

---
+++ Chapter 4/app.js
@@ -0,0 +1,23 @@
+// Question 1
+var name = "Ali", age = 20, isStudent = true;
+// Qusetion 2
+// LEGAL Variable Names:
+
+var userName;
+var $price;
+var _total;
+var myVariable1;
+var first_name;
+// ILLEGAL Variable Names:
+
+var 1name;
+var my-name;
+var var;         
+var first name; 
+var @value;
+// Question 3
+document.write("Rules for naming JS variables");
+document.write("Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable ");
+document.write("Variables must begin with a letter, $ or _. For example $name, _name or name");
+document.write("Variable names are case sensitive <br><br>");
+document.write("Variable names should not be JS keywords");
