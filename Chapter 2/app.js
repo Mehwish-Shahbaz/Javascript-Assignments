e5412d006139ac043ccb042e577be77a20d0ccaf
Mehwish Shahbaz <mahwishahbaz@gmail.com>
2026-03-27 12:15:38 +0500

    Chapter 2 Done

---
+++ Chapter 2/app.js
@@ -0,0 +1,31 @@
+let userName 
+console.log(userName)
+
+let myName = "Mahrosh Hafsa"
+console.log(myName)
+
+let message = "Hello World!"
+alert(message)
+let studentName = "Jhon Doe"
+
+let studentAge = 15 + " years old"
+
+let studentCourse = "Certified Mobile Application Development"
+alert(studentName)
+alert(studentAge)
+alert(studentCourse)
+
+let food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(food)
+let email = "mahroshhafsa@gmail.com";
+
+alert("My email address is " + email);
+
+let book = "A smarter way to learn JavaScript"
+
+alert("I am trying to learn from the book " + book)
+
+document.write("Yah! I can write HTML content through JavaScript")
+
+let pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(pattern)
